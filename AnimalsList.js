a6544c88b23a42bef236638aec4b809a76f50cde
Polina Platonova <157739090+pplatonn@users.noreply.github.com>
2025-01-06 21:56:07 +0300

    comments to each component were added

--- AnimalsList.js
+++ AnimalsList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// function Pet(props) {
-//     return <li>{props.showPet}</li>
-// }
+function Pet(props) {
+    return <li>{props.showPet}</li>
+}
+
+// if it's needed to take items from an array, use this: 
 
 // function AnimalsList() {
 //     return (
@@ -16,38 +18,42 @@ import ReactDOM from 'react-dom/client';
 //     )
 // }
 
-// function AnimalsList() {
-//     return (
-//         <>
-//             <h1>You have:</h1>
-//             <ul>
-//                 {petsArr.map((pet) => <Pet key={pet.id} showPet={pet.showPet}/>)}
-//             </ul>
-//         </>
-//     )
-// }
-
-
-// const petsArr = [
-//     {
-//         "id": 1,
-//         "showPet": "redCat"
-//     },
-//     {
-//         "id": 3,
-//         "showPet": "dog"
-//     },
-
-//     {
-//         "id": 2,
-//         "showPet": "brownCat"
-//     },
-
-// ]
-
 // const petsArr = ["redCat", "brownCat", "dog"];
 // const pets = ["redCat"];
 // const pets = [];
+
+// ************ //
+
+
+// if you pass an object, use this:
+
+function AnimalsList() {
+    return (
+        <>
+            <h1>You have:</h1>
+            <ul>
+                {petsArr.map((pet) => <Pet key={pet.id} showPet={pet.showPet}/>)}
+            </ul>
+        </>
+    )
+}
+
+const petsArr = [
+    {
+        "id": 1,
+        "showPet": "redCat"
+    },
+    {
+        "id": 3,
+        "showPet": "dog"
+    },
+
+    {
+        "id": 2,
+        "showPet": "brownCat"
+    },
+
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(< AnimalsList />);
